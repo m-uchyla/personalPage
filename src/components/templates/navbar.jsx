cb6c1adf62597ef07e5cebdf17b202ed1bef200f
m-uchyla <czarnytroll@gmail.com>
2023-08-21 12:31:09 +0200

    Tailwind init & default nav design

--- src/components/templates/navbar.jsx
+++ src/components/templates/navbar.jsx
@@ -3,8 +3,26 @@
 export default function Navbar() {
 
     return(
-        <nav>
-            <h1>Test</h1>
+        <nav className="flex w-full justify-evenly">
+            <div className="text-center flex items-center">
+                <h1>Logo</h1>
+            </div>
+            <div>
+                <ul className="flex">
+                    <li>
+                        <a href="/">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="/">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="/">Link 3</a>
+                    </li>
+                    <li>
+                        <a href="/">Link 4</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
