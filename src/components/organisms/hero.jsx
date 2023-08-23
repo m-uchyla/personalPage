d881292a172fe3a4284b6356a9ae2f3a508685b2
m-uchyla <czarnytroll@gmail.com>
2023-08-23 12:15:49 +0200

    Hero first concept

--- src/components/organisms/hero.jsx
+++ src/components/organisms/hero.jsx
@@ -2,8 +2,15 @@
 export default function Hero(){
 
     return(
-        <section id="hero">
-            <h1>Test</h1>
+        <section id="hero" className="flex h-[700px] items-center gap-10">
+            <div>
+                <h3 className="text-[20px]">Cześć, jestem</h3>
+                <h1 className="text-[32px] font-[700]">Mateusz Uchyła</h1>
+                <h2 className="text-[20px]">Web developer</h2>
+            </div>
+            <div>
+                <h1>Zdjęcie</h1>
+            </div>
         </section>
     )
 }
