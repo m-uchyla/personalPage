15f1a5e28c9d230250fc0dbd496b8d72653690ba
m-uchyla <czarnytroll@gmail.com>
2023-08-23 14:26:47 +0200

    Card design fix

--- src/components/atoms/card.jsx
+++ src/components/atoms/card.jsx
@@ -2,11 +2,11 @@
 export default function Card(props){
     
     return (
-        <div className={`${props.className} relative`}>
-            <div className={`sm:p-10 p-6 bg-twhite rounded-[25px]  flex-1 z-[10] w-full absolute top-0 left-0`}>
+        <div className={`${props.className} relative mt-[6px]`}>
+            <div className={`sm:p-10 p-6 bg-twhite rounded-[25px] absolute w-full top-[-6px] left-0 flex-1 z-[10]`}>
                 {props.children}
             </div>
-            <div className={`sm:p-10 p-6 bg-[#e1e6ec] rounded-[25px] flex-1 shadow-xl absolute w-full top-[6px] left-0 z-[1]`}>
+            <div className={`sm:p-10 p-6 bg-[#e1e6ec] rounded-[25px] flex-1 shadow-xl  z-[1]`}>
                 {props.children}
             </div>
         </div>
