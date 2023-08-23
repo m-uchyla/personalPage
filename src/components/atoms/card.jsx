c29340288590c7358dddce03e0df6ee383e7280f
m-uchyla <czarnytroll@gmail.com>
2023-08-23 12:54:41 +0200

    Card final design

--- src/components/atoms/card.jsx
+++ src/components/atoms/card.jsx
@@ -2,8 +2,11 @@
 export default function Card(props){
     
     return (
-        <div className={`${props.className}`}>
-            <div className={`sm:p-10 p-6 bg-twhite rounded-[25px] shadow-2xl flex-1`}>
+        <div className={`${props.className} relative`}>
+            <div className={`sm:p-10 p-6 bg-twhite rounded-[25px]  flex-1 z-[10] w-full absolute top-0 left-0`}>
+                {props.children}
+            </div>
+            <div className={`sm:p-10 p-6 bg-[#e1e6ec] rounded-[25px] flex-1 shadow-xl absolute w-full top-[6px] left-0 z-[1]`}>
                 {props.children}
             </div>
         </div>
