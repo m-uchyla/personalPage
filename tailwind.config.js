3e8c018c57337760a67785a79b56696d386565e5
m-uchyla <czarnytroll@gmail.com>
2023-08-23 12:01:32 +0200

    Color pallete & global color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tprimary: "#29407D",
+        tblack: "#080D19",
+        twhite:"#F7F8FA",
+        tsecondary:"#7782A1",
+        tgray:"#DFE3EC",
+      },
+    },
+    screens: {
+      xxs: "330px",
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
